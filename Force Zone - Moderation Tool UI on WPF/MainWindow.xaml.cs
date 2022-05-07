bb8ab35c8d09a48f247bad9bb9803c2061fad5fe
Красный Чай <red__tea@outlook.com>
2022-05-07 23:08:19 +0300

    Added base style for login window

--- Force Zone - Moderation Tool UI on WPF/MainWindow.xaml.cs
+++ Force Zone - Moderation Tool UI on WPF/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +23,66 @@ namespace Force_Zone___Moderation_Tool_UI_on_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int failed = 0;
+        private static bool timeOut;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (failed > 3)
+            {
+                if(!timeOut)
+                {
+                    rez.Text = "Вы совершили слишком много безуспешных\n попыток входа!\n Подождите перед следующей\n попыткой!";
+                    return;
+                } else SetTimer();
+            }
+            if (!Login(login.Text, password.Password))
+            {
+                failed++;
+                rez.Text = "Неверный пароль!";
+                return;
+            }
+            else rez.Text = "Успешный вход!";
+        }
+
+        private bool Login(string login, string password)
+        {
+            var wb = new WebClient();
+            return wb.DownloadString($"https://moderation-tool.forcezone.ru/api/login/get?login={login}&password={password}&modtool=true") switch
+            {
+                "true" => true,
+                _ => false
+            };
+        }
+
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            var aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            timeOut = true;
+        }
+
+        private void SiteRedirect_Click(object sender, RoutedEventArgs e)
+        {
+            var destinationurl = "https://moderation-tool.forcezone.ru/";
+            var sInfo = new System.Diagnostics.ProcessStartInfo(destinationurl)
+            {
+                UseShellExecute = true,
+            };
+            System.Diagnostics.Process.Start(sInfo);
         }
     }
 }
