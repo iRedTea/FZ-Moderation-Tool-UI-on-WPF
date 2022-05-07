9b2641f7bea27f48ac37a789891af5d69d579fb5
denis <denis@USER-PC>
2022-05-07 17:36:16 +0300

    Добавьте файлы проекта.

---
+++ Force Zone - Moderation Tool UI on WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Force_Zone___Moderation_Tool_UI_on_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
